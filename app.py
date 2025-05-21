8a2a4a0f00e4e838d4d7bba3609304b12e50ebe9
Boaz11Jachin <sheepo84k@gmail.com>
2025-05-21 18:04:20 +0900

    DB 연동 (mysql pymysql prophet)

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # pip install numpy
 # pip install pandas
 # pip install flask-cors
+# pip install prophet sqlalchemy pysql
 
 # flask 실행시키려면, 터미널에 flask run
 from flask import Flask, request
@@ -9,6 +10,7 @@ from flask_cors import CORS
 from flask_cors import cross_origin
 import util
 import util2
+import predict
 
 app = Flask(__name__)
 CORS(app, origins="*")   # api 전체
@@ -30,8 +32,16 @@ def health_info() :
 
     return response
 
+@app.get("/api/predict/<region>")
+def predict_handle(region) :
+    df = predict.load_region_data(region)
+    result = predict.run_prophet(df)
+    to_dict = result.to_dict()
 
+    xlabels = [v  for v in to_dict["ds"].values() ]
+    data = [v for v in to_dict["yhat"].values() ]
 
+    return { "xlabels" : xlabels, "data" : data }, 200
 
 
 
